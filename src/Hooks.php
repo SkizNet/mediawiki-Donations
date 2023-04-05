8422b1c974175bb090bde6a67765e914d03a11de
Ryan Schmidt <skizzerz@skizzerz.net>
2023-04-05 10:38:05 -0700

    Initial commit

---
+++ src/Hooks.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MediaWiki\Extensions\Donations;
+
+use MediaWiki\Skins\Hook\SkinAfterPortletHook;
+use OOUI\ButtonWidget;
+use Skin;
+
+class Hooks implements SkinAfterPortletHook {
+	/**
+	 * Displays the HTML donate button in the sidebar when the `sitesupport`
+	 * portlet is defined.
+	 *
+	 * @param Skin $skin
+	 * @param string $portletName
+	 * @param string &$html
+	 * @return void
+	 */
+	public function onSkinAfterPortlet( $skin, $portletName, &$html ) {
+		if ( $portletName === 'sitesupport' ) {
+			$skin->getOutput()->enableOOUI();
+			$skin->getOutput()->addModuleStyles( [ 'ext.donations.icons' ] );
+			$html = new ButtonWidget( [
+				'label' => wfMessageFallback( 'sitesupport-button', 'sitesupport' )->plain(),
+				'href' => wfMessage( 'sitesupport-url' )->plain(),
+				'icon' => 'heart',
+				'flags' => [ 'primary', 'progressive' ]
+			] );
+		}
+	}
+}
